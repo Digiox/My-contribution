d68c492f6d7f2a2877c8396278758d014590e6fa
Lyann <bourlon.lyann84@gmail.com>
2020-04-02 14:13:18 +0200

    fix(ErrorLoadingDatas.jsx): onLoaded props triggered i the useEffect hook
    close #48

--- src/components/Errors/ErrorLoadingDatas.jsx
+++ src/components/Errors/ErrorLoadingDatas.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const ErrorLoadingDatas = () => {
+const ErrorLoadingDatas = ({ onLoaded }) => {
+    useEffect(() => {
+        onloaded()
+    })
     return (
         <div>
             <h2>Nous avons rencontrés des problèmes lors de la collecte de vos données</h2>
