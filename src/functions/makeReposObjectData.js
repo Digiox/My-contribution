811d2c2b98ce0be783ac533bc683463d8d86af4d
Lyann <bourlon.lyann84@gmail.com>
2020-04-07 16:37:02 +0200

    fix(makeRepoObjectData.js): the function don't handle the reject
    close #38

--- src/functions/makeReposObjectData.js
+++ src/functions/makeReposObjectData.js
@@ -1,5 +1,6 @@
 export default function makeObjectData(userName, repos) {
     return new Promise(function (resolve, reject) {
+        if (!userName || !repos) reject("undefined parameters")
         resolve({
             'userName': userName,
             'repos': repos
