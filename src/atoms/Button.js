aa9879f770afe2d07c00491786ac589eb4b59079
Digiox <xlt84400@gmail.com>
2020-05-21 17:57:34 +0200

    fix(propTypes): Added propTypes in Button.js & Img.jsx

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Button = ({ children, ...props }) => (
     <button { ...props }>{ children }</button>
 )
+
+Button.prototype = {
+    children: PropTypes.string.isRequired,
+    onClick: PropTypes.func
+}
 
 export default Button
--- src/atoms/Img.jsx
+++ src/atoms/Img.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import PropTypes from "prop-types"
 
 const Img = ({src, alt}) => {
     return ( <img src={src} alt={alt} /> );
+}
+
+Img.prototype = {
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
 }
  
 export default Img;
